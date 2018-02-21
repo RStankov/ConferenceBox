0ec66f19d420cd19ba2bef83d34c37fbf85f4192
Radoslav Stankov <rstankov@gmail.com>
2018-02-21 23:06:30 +0200

    Extract method

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,11 +3,17 @@
 module AdminHelper
   def upload_input(form, field_name)
     attachment = form.object.public_send(field_name)
-    preview = content_tag(:div, link_to(image_tag(attachment.variant(resize: '50x'), width: 50), attachment, target: :blank)) if attachment.attached?
+    preview = image_preview attachment if attachment.attached?
     input = form.input(:logo, as: :file, label: false, wrapper_html: { style: 'display: block !important;' })
 
     field_set_tag field_name.to_s.humanize do
       "#{preview}#{input}".html_safe
     end
+  end
+
+  private
+
+  def image_preview(attachment)
+    content_tag :div, link_to(image_tag(attachment.variant(resize: '50x'), width: 50), attachment, target: :blank)
   end
 end
