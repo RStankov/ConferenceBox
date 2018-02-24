5b59f1c06ed579369ce10a68a0be4bd6f9d321e8
Radoslav Stankov <rstankov@gmail.com>
2018-02-24 11:40:04 +0200

    Have smart resize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,11 +48,17 @@ module ApplicationHelper
     return unless attachment.attached?
 
     if size.present?
-      image_tag attachment.variant(resize: "#{size * 2}x#{size * 2}!"), size: size, **args
+      image_tag attachment.variant(smart_resize(size * 2)), size: size, **args
     elsif height.present?
       image_tag attachment.variant(resize: "x#{height * 2}"), height: height, **args
     else
       image_tag attachment, args
     end
+  end
+
+  private
+
+  def smart_resize(size)
+    { resize: "#{size}x#{size}^", gravity: 'center', crop: "#{size}x#{size}+0+0" }
   end
 end
