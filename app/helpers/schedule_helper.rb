989b7eb9d2ce649c44c7d9340c2725f79b8a762f
Radoslav Stankov <rstankov@gmail.com>
2018-04-18 22:30:42 +0200

    Add sponsors to schedule json

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -2,12 +2,39 @@
 
 module ScheduleHelper
   def render_schedule_json(event)
-    return {} unless event.sessions_announced?
-
-    event.sessions.map { |s| ScheduleSessionSerializer.new(s) }.group_by(&:track).to_json
+    EventSerializer.new(event).to_json
   end
 
-  class ScheduleSessionSerializer
+  class EventSerializer
+    attr_reader :event
+
+    def initialize(event)
+      @event = event
+    end
+
+    def as_json(_options = {})
+      return {} unless event.sessions_announced?
+
+      {
+        sessions: sessions,
+        sponsors: sponsors
+      }
+    end
+
+    private
+
+    def sessions
+      return [] unless event.sessions_announced?
+      event.sessions.map { |s| SessionSerializer.new(s) }.group_by(&:track)
+    end
+
+    def sponsors
+      return [] unless event.sponsors_announced?
+      event.sponsors.map { |s| SponsorSerializer.new(s) }
+    end
+  end
+
+  class SessionSerializer
     attr_reader :session
 
     delegate :track, to: :session
@@ -18,29 +45,44 @@ module ScheduleHelper
 
     def as_json(_options = {})
       {
-        title:     session.title,
-        startTime: start_time,
-        speakers:  speakers_hash
+        title: session.title,
+        startTime: time(session.start_at),
+        endTime: time(session.end_at),
+        speakers: speakers
       }
     end
 
     private
 
-    def start_time
+    def time(hour)
+      return unless hour.present?
+      hour, minute = hour.split(':')
       date = session.event.date
-
-      hour, minute = session.start_at.split(':')
-
       Time.new(date.year, date.month, date.day, hour.to_i, minute.to_i)
     end
 
-    def speakers_hash
+    def speakers
       session.speakers.map do |speaker|
         {
-          name:         speaker.name,
-          description:  speaker.description,
+          name: speaker.name,
+          avatarUrl: Attachment.url(speaker.photo, size: 350),
         }
       end
+    end
+  end
+
+  class SponsorSerializer
+    attr_reader :sponsor
+
+    def initialize(sponsor)
+      @sponsor = sponsor
+    end
+
+    def as_json(_options = {})
+      {
+        url: sponsor.website_url,
+        logoUrl: Attachment.url(sponsor.logo, height: 60)
+      }
     end
   end
 end
